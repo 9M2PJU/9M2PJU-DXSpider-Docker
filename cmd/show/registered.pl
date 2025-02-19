e049fbb3fa2c84eb997cc07f8d1cda7b78aa20b0
9M2PJU <9m2pju@gmail.com>
2025-02-19 21:30:40 +0800

    update

---
+++ cmd/show/registered.pl
@@ -0,0 +1,63 @@
+#
+# show/registered
+#
+# show all registered users 
+#
+# Copyright (c) 2001 Dirk Koopman G1TLH
+#
+#
+#
+
+my ($self, $line) = @_;
+return (1, $self->msg('e5')) unless $self->priv >= 9;
+
+my @out;
+my @val;
+
+use DB_File;
+
+if ($line) {
+	$line =~ s/[^\w\-\/]+//g;
+	$line = "\U\Q$line";
+}
+
+my %call = ();
+$call{$_} = 1 for split /\s+/, $line;
+delete $call{'ALL'};
+
+my ($action, $count, $key, $data) = (0,0,0,0);
+unless (keys %call) {
+	for ($action = DXUser::R_FIRST, $count = 0; !$DXUser::dbm->seq($key, $data, $action); $action = DXUser::R_NEXT) {
+		if ($data =~ m{registered}) {
+			$call{$key} = 1;       # possible candidate
+		}
+	}
+}
+
+foreach $key (sort keys %call) {
+	my $u = DXUser::get_current($key);
+	if ($u && defined (my $r = $u->registered)) {
+		push @val, "${key}($r)";
+		++$count;
+	}
+	
+}
+
+my @l;
+push @out, "Registration is " . ($main::reqreg ? "Required" :  "NOT Required");
+foreach my $call (@val) {
+	if (@l >= 5) {
+		push @out, sprintf "%-12s %-12s %-12s %-12s %-12s", @l;
+		@l = ();
+	}
+	push @l, $call;
+}
+if (@l) {
+	push @l, "" while @l < 5;
+	push @out, sprintf "%-12s %-12s %-12s %-12s %-12s", @l;
+}
+
+push @out, $self->msg('rec', $count);
+return (1, @out);
+
+
---
+++ cmd/who.pl
@@ -0,0 +1,44 @@
+#
+# who : is online
+# a complete list of stations connected
+#
+# Copyright (c) 1999 Dirk Koopman G1TLH
+#
+#
+
+
+my $self = shift;
+my $dxchan;
+my @out;
+
+push @out, "  Callsign Type      Started           Name     Ave RTT Link";
+
+foreach $dxchan ( sort {$a->call cmp $b->call} DXChannel::get_all ) {
+    my $call = $dxchan->call();
+	my $t = cldatetime($dxchan->startt);
+	my $type = $dxchan->is_node ? "NODE" : "USER";
+	my $sort = "    ";
+	if ($dxchan->is_node) {
+		$sort = "DXSP" if $dxchan->is_spider;
+		$sort = "CLX " if $dxchan->is_clx;
+		$sort = "DXNT" if $dxchan->is_dxnet;
+		$sort = "AR-C" if $dxchan->is_arcluster;
+		$sort = "AK1A" if $dxchan->is_ak1a;
+	} else {
+		$sort = "LOCL" if $dxchan->conn->isa('IntMsg');
+		$sort = "WEB " if $dxchan->is_web;
+		$sort = "EXT " if $dxchan->conn->isa('ExtMsg');
+		$type = "RBN " if $dxchan->is_rbn;              # Yes, this is NOT a typo
+	}
+	my $name = $dxchan->user->name || " ";
+	my $ping = $dxchan->is_node && $dxchan != $main::me ? sprintf("%5.2f", $dxchan->pingave) : "     ";
+	my $conn = $dxchan->conn;
+	my $ip = '';
+	if ($conn) {
+		$ip = $dxchan->hostname;
+		$ip = "AGW Port ($conn->{agwport})" if exists $conn->{agwport};
+	}
+	push @out, sprintf "%10s $type $sort $t %-10.10s $ping $ip", $call, $name;
+}
+
+return (1, @out)
